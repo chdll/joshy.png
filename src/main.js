6c8327ad3e581549fca589c55df1906773b1a6ab
RealGoneKid420 <oscar@thebarnesfamily.uk>
2024-11-02 20:30:49 +0000

    added variable that tracks last mouse movement

--- src/main.js
+++ src/main.js
@@ -51,5 +51,13 @@ app.on('window-all-closed', () => {
   }
 });
 
+var lastMouseMoveTime = Date()
+
+var mouseMoved = function () {
+  lastMouseMoveTime = new Date()
+}
+
+document.addEventListener('mousemove', mouseMoved, false);
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
