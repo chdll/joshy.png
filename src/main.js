024b0257ac110a8dc0d426efad4116869283ddc9
chdll <115464271+chdll@users.noreply.github.com>
2024-11-03 05:02:52 +0000

    IT WORKS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow, webFrame, ipcRenderer, ipcMain, screen } = require('electron');
 const path = require('node:path');
-
+let mainWindow;
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit();
@@ -11,6 +11,7 @@ function fadeout(){
     clearInterval(fader)
     fader = null
   }
+  console.log("try fadeout")
   fader = setInterval(fade, 10, true);
 }
 function fadein(){
@@ -18,37 +19,38 @@ function fadein(){
     clearInterval(fader)
     fader = null
   }
+  console.log("try fadein")
   fader = setInterval(fade, 10, false)
 }
 function fade(fadeout){
-  const win = BrowserWindow.getFocusedWindow();
+  const win = mainWindow;
   const winop = win.getOpacity();
   if (winop <= 0 && fadeout|| winop >= 1 && !fadeout){
     clearInterval(fader);
     fader = null;
   }else{
     if (fadeout){
-      if (winop - 0.01 >= 0){
-        win.setOpacity(winop - 0.01);
-      } else {
-        win.setOpacity(0)
-      }
+      win.setOpacity(winop - 0.01);
     }else{
-      if (winop + 0.01 <= 1){
-        win.setOpacity(winop + 0.01);
-      } else {
-        win.setOpacity(1)
-      }
+      win.setOpacity(winop + 0.01);
     }
   }
+  
 }
-const createWindow = () => {
+
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    ipcMain.on('fadeout', fadeout)
+    ipcMain.on('fadein', fadein)
   // Create the browser window.
   const display = screen.getPrimaryDisplay()
   const {width, height} = display.workAreaSize
   const nheight = height * 0.9
   const nwidth = (nheight/16) * 9
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     nwidth,
     height,
     frame: false,
@@ -57,7 +59,6 @@ const createWindow = () => {
     },
   });
   mainWindow.setPosition(0, 0)
-
   // and load the index.html of the app.
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
@@ -67,16 +68,7 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-  
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-    ipcMain.on('fadeout', fadeout)
-    ipcMain.on('fadein', fadein)
-  createWindow();
+  ;
 
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
