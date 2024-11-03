59a21276d767df8385125de6c23fc15709801b28
zilpe <62509567+zilpe@users.noreply.github.com>
2024-11-03 06:01:32 +0000

    lookingAway functionality

--- src/lib/facetracking.js
+++ src/lib/facetracking.js
@@ -29,7 +29,7 @@ function setup() {
     bodyPose.detectStart(video, gotPoses)
 }
 
-
+let lookingAway;
 
 function draw() {
     image(video, 0, 0)
@@ -52,21 +52,68 @@ function draw() {
                 circle(keypoint.x, keypoint.y, 12)
             }
 
-            if (pose.left_ear.x > pose.left_eye.x) {
-                console.log('looking LEFT')
-            }
+            // if (pose.left_ear.x > pose.left_eye.x) {
+            //     console.log('looking LEFT')
+            //     lookingAway = true
+            // }
 
-            if (pose.right_ear.x < pose.right_eye.x) {
-                console.log('looking RIGHT')
-            }
+            // if (pose.right_ear.x < pose.right_eye.x) {
+            //     console.log('looking RIGHT')
+            //     lookingAway = true
+            // }
 
             // if (pose.nose.y - 21 < pose.left_eye.y || pose.nose.y - 21 < pose.right_eye.y) {
             //     console.log('looking UP')
             // }
 
-            if (Math.min(pose.left_ear.y, pose.right_ear.y) < Math.max(pose.left_eye.y, pose.right_eye.y)) {
-                console.log('looking DOWN')
+            // if (Math.min(pose.left_ear.y, pose.right_ear.y) < Math.max(pose.left_eye.y, pose.right_eye.y)) {
+            //     console.log('looking DOWN')
+            //     lookingAway = true
+            // }
+
+
+            if ((pose.left_ear.x > pose.left_eye.x) || (pose.right_ear.x < pose.right_eye.x) || (Math.min(pose.left_ear.y, pose.right_ear.y) < Math.max(pose.left_eye.y, pose.right_eye.y))) {
+                lookingAway = true
+                console.log('lookingAway')
+            } else {
+                lookingAway = false
             }
         }
     }
 }
+
+// let lookAwayStartTime = null;
+// let attentionStartTime = null;
+// let distractedTrigger = false;
+
+// function checkAttention() {
+//     if (lookingAway) {
+//         attentionStartTime = null;
+
+//         if (lookAwayStartTime === null) {
+//             lookAwayStartTime = Date.now()
+//         }
+
+//         if (Date.now() - lookAwayStartTime >= 5000 && !distractedTrigger) {
+//             // distractedUser()
+//             console.log('subway')
+//             lookAwayStartTime = null;
+//             distractedTrigger = true;
+//         } else {
+//             if (attentionStartTime === null) {
+//                 attentionStartTime = Date.now()
+//             }
+//         }
+
+//         if (distractedTrigger && Date.now() - attentionStartTime >= 10000) {
+//             lookAwayStartTime = null;
+//             distractedTrigger = false;
+//             attentionStartTime = null;
+//             console.log('user has paid attention for 10 secs')
+//         }
+//     } else {
+//         lookAwayStartTime = null;
+//         attentionStartTime = Date.now()
+//     }
+// }
+// setInterval(checkAttention, 500)
